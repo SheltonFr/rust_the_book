29ea6f3fa897ae3d66bcea57a9854dee5ea8e043
Shelton Francisco <ssalicuchepa@technoplus.co.mz>
2024-04-26 18:03:35 +0200

    shadowing

---
+++ 3.common_programming_concepts/variables/src/main.rs
@@ -0,0 +1,14 @@
+const PROJECT_OWNER: &str = "Shelton Francisco";
+
+fn main() {
+    println!("Hello, {PROJECT_OWNER}!");
+    let mut x = 5;
+    println!("The values of x is: {x}");
+    x = 6;
+    println!("The values of x is: {x}");
+
+    let shadow = " ";
+    let shadow = shadow.len();
+
+    println!("The value of shadow is: {shadow}");
+}
